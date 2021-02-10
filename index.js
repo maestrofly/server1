2e46b947e0aff11780ba729a0ea1e65883788d5b
DerrickDarku <derrickalberto@ymail.com>
2021-02-10 23:08:19 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const path = require('path');
 
-const server = express();
+const server = express('server');
 
-const handleAllTypesOfRequests = (req, res) => {
-   res.send("Response from server.use");
-}
+// //middleware definitions
+server.use(express.static(path.join(__dirname,'public')))
+
+//  const serveHomePage = (req, res) =>{
+//    //1. find the file
+//    const homepageFilePath = path.join(__dirname, 'public', 'index.html'); 
+//    //2. send file to client
+//    res.sendFile(homepageFilePath);
+//  }
+
+//  const serveprofilePage =(req, res)=>{
+//   const profilePageFilePath = path.join(__dirname,'public', 'profile.html');
+//    res.sendFile(profilePageFilePath);
+  
+//  } 
+
+
+//route definitions
+
+// server.get('/', serveHomePage);
+// server.get('/profile', serveprofilePage);
+
+server.listen(3000,()=>console.log('MAESTROS SERVER IS READY'));
+
+
+// const handleAllTypesOfRequests = (req, res) => {
+//    res.send("Response from server.use");
+// }
  
-server.use('/profile', handleAllTypesOfRequests);
-server.get('/login', (req, res) => res.send('Hello this is the login page'));
+// server.use('/profile', handleAllTypesOfRequests);
+// serv er.get('/login', (req, res) => res.send('Hello this is the login page'));
 
-server.listen(3000, () => console.log('server is ultra ready'));
+// server.listen(3000, () => console.log('server is ultra ready'));
