dbbcb019865380de612c9fa5eba88e5258a67760
DerrickDarku <derrickalberto@ymail.com>
2021-02-11 23:27:56 +0800

    Parsing body

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const server = express('server');
 
+const loginRequestHandler = (req, res) =>{
+    // let body = "";
+    // req.on('data', chunk =>{
+    //     body += chunk;
+    
+    // });
+    // req.on('end', ()=> {
+    //     //parse the data with RegEx
+    //     console.log(body);
+    // })
+    console.log(req.body.email);
+    console.log(req.body.password);
+    
+    
+    res.send('DONE');
+}
+
+const SignupRequestHandler = (req, res) =>{
+    console.log(req.body.name);
+    console.log(req.body.email);
+    console.log(req.body.password);
+
+    res.send('SIGN UP')
+}
+
+
 // //middleware definitions
 server.use(express.static(path.join(__dirname,'public')))
+server.use(bodyParser.urlencoded({extended: false}))
+
+//route
+server.post('/login', loginRequestHandler)
+server.post('/signup', SignupRequestHandler)
+
+
 
 //  const serveHomePage = (req, res) =>{
 //    //1. find the file
@@ -18,12 +52,23 @@ server.use(express.static(path.join(__dirname,'public')))
 //    res.sendFile(profilePageFilePath);
   
 //  } 
+const serveContactPage = (req, res) => {
+    const ContactPageFilePath = path.join(__dirname, 'public', 'contact.html' );
+    res.sendFile(serveContactPage);
+}
+
+const serveAboutPage = (req, res) => {
+    const AboutPageFilePath = path.join(__dirname, 'public', 'about.html');
+    res.sendFile(serveAboutPage);
+}
 
 
 //route definitions
 
 // server.get('/', serveHomePage);
 // server.get('/profile', serveprofilePage);
+server.get('/contact', serveContactPage);
+server.get('/about', serveAboutPage);
 
 server.listen(3000,()=>console.log('MAESTROS SERVER IS READY'));
 
